7111ec772f15faf62d0701d01fd8b4e944762932
Adam Elwood <aelwood@cmselwoodmb.desy.de>
2018-10-17 15:10:36 +0200

    updated slides and added conda setup

---
+++ init_virtualenv_conda.sh
@@ -0,0 +1,10 @@
+conda create --name tensorflowTutorial
+source activate tensorflowTutorial
+conda install tensorflow
+conda install keras
+conda install h5py
+conda install pypng
+conda install sklearn
+conda install matplotlib
+conda install numpy
+conda install jupyter
--- slides/run_pandoc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pandoc -t beamer -s -fmarkdown-implicit_figures --template=template.beamer slides.md -o slides.pdf
